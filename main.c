d0c5fcf8d3e1abd33a1e6eaa44e42022f60f5227
André Augusto Moreira Martins <64978311+AndreMartins21@users.noreply.github.com>
2023-12-02 13:00:41 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/*
+=> ALGORITMO PARA O JOGO:
+
+1. Desenhar o cenário.
+2. Criar a estrutura da nave.
+3. Desenhar uma nave na tela.
+4. Implementar o movimento dessa nave.
+5. Implementar o desenho e o movimento da segunda nave.
+6. Implementar os diferentes tipos de naves e fazer com que as naves troquem de tipo aleatoriamente ao
+atravessar a tela.
+7. Implementar a troca aleatória de posição vertical das naves.
+8. Criar a estrutura para o tiro.
+9. Implementar o disparo do tiro: desenho, disparo a partir da tecla e movimento.
+10. Implementar a estrutura para o alvo.
+11. Criar a matriz de alvos e inicializar os seus valores de acordo com o que é pedido neste enunciado.
+12. Desenhar os alvos na tela.
+13. Implementar a colisão do tiro com o alvo.
+14. Implementar o sistema de pontuação.
+15. Exibir a pontuação na tela.
+16. Implementar o sistema de vidas (os 3 tiros que cada jogador pode errar).
+17. Exibir as vidas na tela.
+18. Implementar o fim de jogo.
+19. Exibir uma tela de fim de jogo na tela.
+20. Implementar sistema de registro do histórico de partidas.
+21. Exibir o histórico das partidas na tela de fim de jogo.
+22. Escrever a documentação.
+*/
+
+
+void main(){
+    printf("Hello, World");
+}
